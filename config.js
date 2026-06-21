8f08a429b033aba89b262cfee973d4d2d5727007
rckwd <key.grunge@gmail.com>
2026-06-21 15:11:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// config.js
-const APP_URL = "https://script.google.com/macros/s/AKfycbwa0TkNJEaxsQ41EsiCjvI-gJ1-zL7BZEbdkYIMMOkFZ58PooFUEzX5mp0MLVZtKOxn9g/exec";
+// config.jsht
+const APP_URL = "https://script.google.com/macros/s/AKfycbwnA2z7K5gXyGvv5_n44Xf_UqtJKsXbnGwwtsXqXoBjj0-oq8URkwpj0aZkB13kj017/exec";
